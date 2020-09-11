95953f4ef7f45623aa84c3f78b01043f1e66fa0f
vorSherer <tcpma2011@gmail.com>
2020-09-10 21:56:59 -0700

    Working code prior to Dockerization.

--- books/views.py
+++ books/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .serializer import BookSerializer
+from .models import Book
+
+
+class BookList(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
